47c845c39653d47574a49ed0fcff90b2ca1ff0cd
Mauriaguilar7 <00195721@uca.edu.sv>
2023-06-08 23:33:06 -0600

    Agregando Vistas Recuperar Contraseña (email y verificacion de codigo)

---
+++ app/src/main/java/com/example/trabajitosinc/EmailFragment.kt
@@ -0,0 +1,24 @@
+package com.example.trabajitosinc
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.trabajitosinc.databinding.FragmentEmailBinding
+
+
+class EmailFragment : Fragment() {
+
+    private lateinit var binding : FragmentEmailBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentEmailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+}
