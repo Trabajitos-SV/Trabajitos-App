a9a3c88d5cb1ad8f0f3c00950cf7812507893c4a
Mauriaguilar7 <00195721@uca.edu.sv>
2023-06-13 22:12:35 -0600

    Agregando: Navegación en login

--- app/src/main/java/com/example/trabajitosinc/TrabajitosApplication.kt
+++ app/src/main/java/com/example/trabajitosinc/TrabajitosApplication.kt
@@ -4,7 +4,6 @@ import android.app.Application
 import com.example.trabajitosinc.data.categories
 
 import com.example.trabajitosinc.data.users
-import com.example.trabajitosinc.repositories.CategoryRepository
 import com.example.trabajitosinc.repositories.UserRepository
 import com.example.trabajitosinc.data.trabajitos
 import com.example.trabajitosinc.repositories.CategoryRepository
--- app/src/main/java/com/example/trabajitosinc/ui/LoginFragment.kt
+++ app/src/main/java/com/example/trabajitosinc/ui/LoginFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.trabajitosinc.R
 import com.example.trabajitosinc.databinding.FragmentLoginBinding
 
 
@@ -20,6 +22,18 @@ class LoginFragment : Fragment() {
     ): View {
         binding = FragmentLoginBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.textForgotPassword.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_emailFragment)
+        }
+
+        binding.loginButton.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_mainActivity)
+        }
     }
 
 }
