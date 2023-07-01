292a808b220ed47afaf0ce2165a06767e4f2bc96
michaelnarvaez <leemichaeln24@gmail.com>
2023-07-01 16:32:18 -0600

    Añadiendo los reponse de create portfolio combinado con revirews

--- app/src/main/java/com/example/trabajitosinc/network/dto/portfolio/createPortfolio/CreatePortfolioResponse.kt
+++ app/src/main/java/com/example/trabajitosinc/network/dto/portfolio/createPortfolio/CreatePortfolioResponse.kt
@@ -1,5 +1,17 @@
 package com.example.trabajitosinc.network.dto.portfolio.createPortfolio
 
 data class CreatePortfolioResponse (
-    val
-        )
+    val _id: String,
+    val title: String,
+    val description: String,
+    val user: String,
+    val category: String,
+    val images: List<String>,
+    val reviews: List<reviewResponse> = emptyList()
+)
+
+data class reviewResponse(
+    val review: String,
+    val qualification: Int,
+    val portfolioId: String,
+)
