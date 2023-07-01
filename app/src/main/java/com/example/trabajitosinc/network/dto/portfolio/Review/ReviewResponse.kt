e57ed46c41ebbcadb0e9f9dac75276f69d9e12af
michaelnarvaez <leemichaeln24@gmail.com>
2023-07-01 16:52:46 -0600

    Agregando Review request-reponse

---
+++ app/src/main/java/com/example/trabajitosinc/network/dto/portfolio/Review/ReviewResponse.kt
@@ -0,0 +1,7 @@
+package com.example.trabajitosinc.network.dto.portfolio.Review
+
+data class ReviewResponse (
+    val description: String,
+    val qualification: Int,
+    val id_user: String
+    )
