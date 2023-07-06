005ea528149267322e3c3811dc5751b18d4cd0f9
michaelnarvaez <leemichaeln24@gmail.com>
2023-07-05 19:15:52 -0600

    Eliminando Portfolio request y reponse, estaban sobreescritos

--- app/src/main/java/com/example/trabajitosinc/network/dto/portfolio/PortfolioRequest.kt
+++
@@ -1,9 +0,0 @@
-package com.example.trabajitosinc.network.dto.portfolio
-
-data class PortfolioRequest(
-    val title: String,
-    val description: String,
-    val image: String,
-    val category: String
-)
-
