d8b6c5400b810e624d853c6b17056c6c6188cbe3
michaelnarvaez <leemichaeln24@gmail.com>
2023-07-05 19:14:59 -0600

    Modificando los metodos de las rutas en portfolio

--- app/src/main/java/com/example/trabajitosinc/network/service/PortfolioService.kt
+++ app/src/main/java/com/example/trabajitosinc/network/service/PortfolioService.kt
@@ -1,6 +1,8 @@
 package com.example.trabajitosinc.network.service
 
-import com.example.trabajitosinc.network.dto.portfolio.PortfolioResponse
+import com.example.trabajitosinc.network.dto.portfolio.createPortfolio.CreatePortfolioResponse
+import com.example.trabajitosinc.network.dto.portfolio.findAllPortfolios.FindAllPortfolioResponse
+import com.example.trabajitosinc.network.dto.portfolio.findMyPortfolio.FindMyPortfolioResponse
 import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.PATCH
@@ -9,20 +11,14 @@ import retrofit2.http.POST
 interface PortfolioService {
 
     @GET("portfolio/myPortfolio")
-    suspend fun findMyPortfolio(): List<PortfolioResponse>
+    suspend fun findMyPortfolio(): FindMyPortfolioResponse
 
     @GET("portfolio")
-    suspend fun findAllPortfolio(): List<PortfolioResponse>
+    suspend fun findAllPortfolio(): FindAllPortfolioResponse
 
     @POST("portfolio")
-    suspend fun createPortfolio(): PortfolioResponse
+    suspend fun createPortfolio(): CreatePortfolioResponse
 
-    @PATCH("portfolio/updateMyPortfolio/:id")
-    suspend fun updateMyPortfolio(): PortfolioResponse
 
-    @DELETE("portfolio/deleteMyPortfolio/:id")
-    suspend fun deleteMyPortfolio(): PortfolioResponse
 
-    @POST("portfolio/review/:id")
-    suspend fun createReview(): PortfolioResponse
 }
