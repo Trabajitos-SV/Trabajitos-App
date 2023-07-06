c09d8b5eb0cd2e41010c12d7f19e662cbc758121
Mauriaguilar7 <00195721@uca.edu.sv>
2023-07-06 02:51:57 -0600

    Agregando Acceder a la galeria

--- app/src/main/java/com/example/trabajitosinc/ui/user/CreatePortfolio/recyclerview/CreatePorfolioRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/trabajitosinc/ui/user/CreatePortfolio/recyclerview/CreatePorfolioRecyclerViewAdapter.kt
@@ -1,16 +1,22 @@
+package com.example.trabajitosinc.ui.user.CreatePortfolio.recyclerview
 
+import CreatePortfolioRecyclerViewHolder
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.trabajitosinc.data.models.PortfolioModel
 import com.example.trabajitosinc.databinding.ItemImagePortfolioBinding
-import com.example.trabajitosinc.ui.user.CreatePortfolio.recyclerview.CreatePortfolioRecyclerViewHolder
 
 class CreatePorfolioRecyclerViewAdapter(
     private val clickListener: (PortfolioModel) -> Unit
 ): RecyclerView.Adapter<CreatePortfolioRecyclerViewHolder>() {
 
     private val portfolios = ArrayList<PortfolioModel>()
+    private var selectedPosition = RecyclerView.NO_POSITION
+    private var lastItemClickListener: (() -> Unit)? = null
+
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CreatePortfolioRecyclerViewHolder {
         val binding = ItemImagePortfolioBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -20,7 +26,15 @@ class CreatePorfolioRecyclerViewAdapter(
     override fun onBindViewHolder(holder: CreatePortfolioRecyclerViewHolder, position: Int) {
             val portfolio = portfolios[position]
             holder.bind(portfolio, clickListener)
+
+            holder.itemView.isSelected = position == selectedPosition
+        // Configurar el clic en el último elemento
+        if (position == itemCount - 1) {
+            holder.itemView.setOnClickListener {
+                lastItemClickListener?.invoke()
+            }
         }
+    }
 
 
     override fun getItemCount(): Int {
@@ -32,5 +46,29 @@ class CreatePorfolioRecyclerViewAdapter(
         portfolios.addAll(portfolioList)
         notifyDataSetChanged()
     }
+
+    fun setSelected(position: Int) {
+        if (position in 0 until portfolios.size) {
+            val previousSelectedPosition = selectedPosition
+            selectedPosition = position
+            notifyItemChanged(previousSelectedPosition)
+            notifyItemChanged(selectedPosition)
+        }
+    }
+
+    // Método para obtener la posición de un elemento en función de la imagen seleccionada
+    fun getItemPosition(selectedImage: Uri): Int {
+        for (i in 0 until portfolios.size) {
+            if (portfolios[i].images == selectedImage.toString()) {
+                return i
+            }
+        }
+        return RecyclerView.NO_POSITION
+    }
+
+    fun setLastItemClickListener(listener: () -> Unit) {
+        lastItemClickListener = listener
+    }
+
 
 }
