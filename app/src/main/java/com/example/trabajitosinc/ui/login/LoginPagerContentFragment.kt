4d7a34e3104d23ded9e8f88eb7b0a4fe0447212d
AngelGuidos <00137321@uca.edu.sv>
2023-07-07 21:47:46 -0600

    Persistencia logIn sin Token

--- app/src/main/java/com/example/trabajitosinc/ui/login/LoginPagerContentFragment.kt
+++ app/src/main/java/com/example/trabajitosinc/ui/login/LoginPagerContentFragment.kt
@@ -1,5 +1,6 @@
 package com.example.trabajitosinc.ui.login
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -12,7 +13,11 @@ import androidx.navigation.fragment.findNavController
 import com.example.trabajitosinc.R
 import com.example.trabajitosinc.TrabajitosApplication
 import com.example.trabajitosinc.databinding.FragmentLoginPagerContentBinding
+import com.example.trabajitosinc.ui.LoginActivity
+import com.example.trabajitosinc.ui.MainActivity
 import com.example.trabajitosinc.ui.login.viewmodel.LoginViewModel
+import com.example.trabajitosinc.util.PreferenceHelper
+import com.example.trabajitosinc.util.PreferenceHelper.set
 
 
 class LoginPagerContentFragment : Fragment() {
@@ -45,19 +50,21 @@ class LoginPagerContentFragment : Fragment() {
             findNavController().navigate(R.id.action_loginFragment_to_mainActivity)
         }
 
-        binding.loginButton.setOnClickListener {
-            it.findNavController().navigate(R.id.action_loginFragment_to_mainActivity)
-        }
-
-
         setViewModel()
 
         observeStatus()
 
+    }
 
-
-
-
+    private fun goToHome() {
+        val i = Intent(context, MainActivity::class.java)
+        creteSessionPreferences()
+        startActivity(i)
+        requireActivity().finish()
+    }
+    private fun creteSessionPreferences() {
+        val preferences = PreferenceHelper.defaultPrefs(requireContext())
+        preferences["session"] = true
     }
 
 
@@ -83,7 +90,8 @@ class LoginPagerContentFragment : Fragment() {
                 loginViewModel.clearStatus()
                 loginViewModel.clearData()
                 app.saveAuthToken(status.token)
-                findNavController().navigate(R.id.action_loginFragment_to_mainActivity)
+                //findNavController().navigate(R.id.action_loginFragment_to_mainActivity)
+                goToHome()
             }
 
             else -> {}
