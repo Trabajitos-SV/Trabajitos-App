e6c5152d7fa43857dbc7c8478951c8bbea934442
AngelGuidos <00137321@uca.edu.sv>
2023-07-08 19:12:57 -0600

    Agregando Parcelaze a los madelos para SafeArgs

--- app/src/main/java/com/example/trabajitosinc/data/models/PortfolioModel.kt
+++ app/src/main/java/com/example/trabajitosinc/data/models/PortfolioModel.kt
@@ -1,5 +1,9 @@
 package com.example.trabajitosinc.data.models
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class PortfolioModel(
     val id: String,
     val title: String,
@@ -8,4 +12,4 @@ data class PortfolioModel(
     val user: UserModel,
     val category: String,
     val review: List<ReviewsModel>
-)
+):Parcelable
--- app/src/main/java/com/example/trabajitosinc/data/models/UserModel.kt
+++ app/src/main/java/com/example/trabajitosinc/data/models/UserModel.kt
@@ -1,5 +1,9 @@
 package com.example.trabajitosinc.data.models
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 class UserModel(
     val id : String,
     val name: String,
@@ -7,4 +11,4 @@ class UserModel(
     val phone: Int,
     val municipality: MunicipalityModel,
     val image: String
-    )
+):Parcelable
