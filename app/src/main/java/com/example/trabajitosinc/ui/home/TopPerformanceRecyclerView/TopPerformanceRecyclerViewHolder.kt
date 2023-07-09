1fbc4db35fed737b98294a5016c30d21dbe4b59d
AngelGuidos <00137321@uca.edu.sv>
2023-07-08 18:48:43 -0600

    Eliminando modelo no usado :wastebasket:

--- app/src/main/java/com/example/trabajitosinc/ui/home/TopPerformanceRecyclerView/TopPerformanceRecyclerViewHolder.kt
+++ app/src/main/java/com/example/trabajitosinc/ui/home/TopPerformanceRecyclerView/TopPerformanceRecyclerViewHolder.kt
@@ -2,7 +2,6 @@ package com.example.trabajitosinc.ui.home.TopPerformanceRecyclerView
 
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.example.trabajitosinc.data.models.RepositoryModel
 import com.example.trabajitosinc.data.models.UserModel
 import com.example.trabajitosinc.databinding.ItemTopPerformanceBinding
 
