c1fcb81b80ac6c27ad6ae01a37d9f7183ae8df1d
michaelnarvaez <leemichaeln24@gmail.com>
2023-07-09 18:17:48 -0600

    Arreglando find category

--- app/src/main/java/com/example/trabajitosinc/ui/user/FindAllPorCategoryId/viewmodel/FindAllCategoryIdViewModel.kt
+++ app/src/main/java/com/example/trabajitosinc/ui/user/FindAllPorCategoryId/viewmodel/FindAllCategoryIdViewModel.kt
@@ -1,4 +1,23 @@
 package com.example.trabajitosinc.ui.user.FindAllPorCategoryId.viewmodel
 
-class FindAllCategoryIdViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.trabajitosinc.network.ApiResponse
+import com.example.trabajitosinc.repository.PortfolioCRepository
+import com.example.trabajitosinc.ui.user.FindAllPorCategoryId.FindAllCategoryIdUiStates
+import kotlinx.coroutines.launch
+
+class FindAllCategoryIdViewModel(private val repository: PortfolioCRepository): ViewModel() {
+
+    val idCategory = MutableLiveData("")
+
+
+    private val _status = MutableLiveData<FindAllCategoryIdUiStates> (FindAllCategoryIdUiStates.Resume)
+
+    val status: LiveData<FindAllCategoryIdUiStates>
+        get() = _status
+
+
 }
