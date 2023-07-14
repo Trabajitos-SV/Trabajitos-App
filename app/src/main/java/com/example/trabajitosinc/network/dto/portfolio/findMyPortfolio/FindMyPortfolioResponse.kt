ad91cfacef7cab3beeda0a09398c7aaca6f686c3
michaelnarvaez <leemichaeln24@gmail.com>
2023-07-13 20:45:15 -0600

    Modificando FindMyPortfolioResponse con los datos correctos!

--- app/src/main/java/com/example/trabajitosinc/network/dto/portfolio/findMyPortfolio/FindMyPortfolioResponse.kt
+++ app/src/main/java/com/example/trabajitosinc/network/dto/portfolio/findMyPortfolio/FindMyPortfolioResponse.kt
@@ -1,11 +1,17 @@
 package com.example.trabajitosinc.network.dto.portfolio.findMyPortfolio
+import com.example.trabajitosinc.data.models.CategoryModel
+import com.example.trabajitosinc.data.models.ImageModel
+import com.example.trabajitosinc.data.models.UserModel
 import com.example.trabajitosinc.network.dto.portfolio.Review.ReviewResponse
+import com.google.gson.annotations.SerializedName
 
 data class FindMyPortfolioResponse (
     val _id: String,
     val title: String,
     val description: String,
-    val user: String,
-    val images: List<String>,
+    val images: List<ImageModel>,
+    val user: UserModel,
+    val category: CategoryModel,
+    @SerializedName("avgQualification") val qualification: Double,
     val reviews: List<ReviewResponse> = emptyList()
 )
